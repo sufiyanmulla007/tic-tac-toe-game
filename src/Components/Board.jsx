e9bb02174793c3e8ed74882020c90fad8bfe2cba
sufyanmulla007 <mohammedsufiyan79@gmail.com>
2023-01-29 11:10:23 +0530

    game

---
+++ src/Components/Board.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../Style/Board.css";
+import Box from './Box';
+const Board = ({board,onClick}) => {
+  return (
+    <>
+    <div className='board'>
+    {board.map((item, idx) => (
+        <Box
+          key={idx}
+          value={item}
+          onClick={() => item == null && onClick(idx)}
+        />
+      ))}
+    
+    </div>
+    </>
+  );
+};
+
+export default Board;
---
+++ src/Components/ScoreBoard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../Style/ScoreBoard.css";
+const ScoreBoard = ({ xScore, oScore, xPlaying, tie }) => {
+  return (
+    <>
+    {/* <h2>tic tac game</h2> */}
+     <div className="scoreboard">
+      <span className={`x-score ${xPlaying === true ? "xScoreboard" : ""}`}>
+       Player X - {xScore}
+      </span>
+      <span className={`o-score ${xPlaying === false ? "oScoreboard" : ""}`}>
+      Player  O - {oScore}
+      </span>
+      <span className={`o-tie ${tie > 0 ? "oTie" : ""}`}>Tie - {tie}</span>
+    </div>
+    </>
+  );
+};
+
+export default ScoreBoard;
